ad0dd919b6d9b3e8fede1f426cfc5785d3508839
Manvendra Kumar <manvendra.kumar7@gmail.com>
2019-02-02 14:33:43 +0530

    Removing files to be untracked

--- projecteuler.py
+++
@@ -1,8 +0,0 @@
-s = 1000
-for a in range(1, s + 1):
-    for b in range(a, s + 1):
-        if 2 * a * b % s != 0:
-            continue
-        c = a + b - 2 * a * b / s
-        if a + b + c == s and c > max(a, b):
-            print a, b, c, a * b * c
