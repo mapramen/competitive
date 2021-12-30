a60bb108ca140ce28e72761d275c99b72e75db09
Manvendra Kumar <manvendra.kumar7@gmail.com>
2021-12-31 00:12:17 +0530

    Round #761: E

--- codeforces/Round #761/christmas_chocolates.cpp
+++ codeforces/Round #761/christmas_chocolates.cpp
@@ -6,9 +6,10 @@ typedef long long ll;
 
 #define pii pair<int,int>
 #define pll pair<ll,ll>
-#define tiiiii tuple<int,int,int,int,int>
 
-map<int,set<int>> adj, arrayIndex;
+priority_queue<int> Q;
+
+map<int,pii> dp;
 
 int Parent(int n){
   int p = 1;
@@ -17,44 +18,11 @@ int Parent(int n){
   return p - n;
 }
 
-void AddToSet(map<int,set<int>>& S, int i, int j){
-  auto it = S.find(i);
-  if(it != S.end()){
-    it->second.insert(j);
+void InitialiseIfNeeded(int p){
+  auto it = dp.find(p);
+  if(it == dp.end()){
+    dp[p] = {INT_MIN, -1};
   }
-  else{
-    S[i] = {j};
-  }
-}
-
-tiiiii DFS(int p){
-  int len = INT_MIN, i = -1, j = -1;
-  int h, ih;
-
-  auto sit = arrayIndex.find(p);
-  if(sit == arrayIndex.end()){
-    h = INT_MIN, ih = -1;
-  }
-  else{
-    set<int>& s = sit->second;
-    
-    h = 0, ih = *s.begin();
-
-    if(s.size() > 1){
-      len = 0, i = *s.begin(), j = *s.rbegin();
-    }
-  }
-
-  for(int x: adj[p]){
-    auto [lenx, ix, jx, hx, ihx] = DFS(x);
-    
-    tie(len, i, j) = max(make_tuple(len, i, j), make_tuple(lenx, ix, jx));
-    tie(len, i, j) = max(make_tuple(len, i, j), make_tuple(h + 1 + hx, ih, ihx));
-
-    tie(h, ih) = max(make_tuple(h, ih), make_tuple(1 + hx, ihx));
-  }
-
-  return {len, i, j, h, ih};
 }
 
 int main(){
@@ -64,17 +32,36 @@ int main(){
   for(int i = 1; i <= n; ++i){
     int x;
     scanf("%d", &x);
-
-    AddToSet(arrayIndex, x, i);
-
-    for(int p = 0; x != 0; x = p){
-      p = Parent(x);
-      AddToSet(adj, p, x);
-    }
+    
+    dp[x] = {0, i};
+    Q.push(x);
   }
 
-  auto [len, i, j, h, ih] = DFS(0);
-  printf("%d %d %d\n", i, j, len);
+  int maxLen = INT_MIN, maxLenI = -1, maxLenJ = -1;
+
+  while(!Q.empty() && Q.top() != 0){
+    int x = Q.top();
+    for( ; !Q.empty() && Q.top() == x; Q.pop());
+
+    int p = Parent(x);
+    InitialiseIfNeeded(p);
+
+    auto [hx, ihx] = dp[x];
+    auto [hp, ihp] = dp[p];
+
+    int len = hx + 1 + hp;
+    if(maxLen < len){
+      maxLen = len, maxLenI = ihx, maxLenJ = ihp;
+    }
+
+    if(hp < 1 + hx){
+      dp[p] = {1 + hx, ihx};
+    }
+
+    Q.push(p);
+  }
+
+  printf("%d %d %d\n", maxLenI, maxLenJ, maxLen);
 
   return 0;
 }
