81d812adb349f74dcfa37a10bf3b06a4acad8c36
Manvendra Kumar <manvendra.kumar7@gmail.com>
2022-01-15 12:57:17 +0530

    SPOJ

---
+++ spoj/test.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+int main(){
+  while(true){
+    int n;
+    scanf("%d", &n);
+    if(n == 42){
+      break;
+    }
+    printf("%d\n", n);
+  }
+  return 0;
+}
