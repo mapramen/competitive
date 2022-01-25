3b6e9df9b831da194a5846bca156e8e0aafaf1f9
Manvendra Kumar <manvendra.kumar7@gmail.com>
2022-01-25 23:22:55 +0530

    spoj

---
+++ spoj/eights.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+ll Solve(){
+  ll n;
+  scanf("%lld", &n);
+  return 192 + (n - 1) * 250;
+}
+
+int main(){
+  int t;
+  scanf("%d", &t);
+  while(t--){
+    printf("%lld\n", Solve());
+  }
+  return 0;
+}
