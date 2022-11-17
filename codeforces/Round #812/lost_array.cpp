27fda1cf807d3bf493c3e37c0c1b9aee17cc02e7
Manvendra Kumar <manvendra.kumar7@gmail.com>
2022-11-17 20:36:23 +0530

    Round #812: F

---
+++ codeforces/Round #812/lost_array.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define N (1 << 20)
+
+int a[N], b[N];
+
+void SubMaskSum(int n){
+  for(int k = __builtin_ctz(n); k != -1; --k){
+    for(int i = 0; i < n; ++i){
+      if((i & (1 << k)) == 0){
+        continue;
+      }
+      a[i] ^= a[(i ^ (1 << k))];
+    }
+  }
+}
+
+void SuperMaskSum(int n){
+  reverse(a, a + n);
+  SubMaskSum(n);
+  reverse(a, a + n);
+}
+
+int main(){
+  int n;
+  scanf("%d", &n);
+
+  for(int i = 0; i < n; ++i){
+    scanf("%d", &a[i]);
+    b[i] = a[i];
+  }
+
+  int m = 1;
+  for( ; m < n; m *= 2);
+
+  reverse(a, a + m);
+
+  SuperMaskSum(m);
+
+  for(int i = 0; i < m - n; ++i){
+    a[i] = 0;
+  }
+
+  SuperMaskSum(m);
+
+  for(int i = n; i < m; ++i){
+    a[i] = b[i];
+  }
+
+  SubMaskSum(m);
+
+  reverse(a, a + n);
+
+  for(int i = 0; i < n; ++i){
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
