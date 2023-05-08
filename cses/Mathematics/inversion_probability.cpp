f9122372623691d22a2a798bbbc835ae4f2133d0
Manvendra Kumar <manvendra.kumar7@gmail.com>
2023-05-08 19:34:34 +0530

    cses

--- cses/Mathematics/inversion_probability.cpp
+++ cses/Mathematics/inversion_probability.cpp
@@ -1,58 +1,60 @@
 #include <stdio.h>
-#include <iostream>
 #include <stdlib.h>
 #include <string.h>
-#include <vector>
-#include <map>
-#include <set>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <cmath>
-#include <utility>
 #include <algorithm>
 #include <bitset>
-#include <climits>
-#include <random>
-#include <chrono>
 #include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 typedef long long ll;
 
-#define pii pair<int,int>
-#define pll pair<ll,ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
 #define N 101
+#define PRECISION 0.000001
 
 int a[N];
 
-int NumberOfInversePair(int x, int y){
-  if(x <= y){
+int NumberOfInversePair(int x, int y) {
+  if (x <= y) {
     return x * (x - 1) / 2;
   }
 
   return y * (x - y) + NumberOfInversePair(y, y);
 }
 
-double ProbabilityOfInversePair(int x, int y){
+double ProbabilityOfInversePair(int x, int y) {
   return 1.0 * NumberOfInversePair(x, y) / (x * y);
 }
 
-int main(){
+int main() {
   int n;
 
   cin >> n;
-  for(int i = 1; i <= n; ++i){
+  for (int i = 1; i <= n; ++i) {
     cin >> a[i];
   }
 
   double ans = 0;
-  for(int i = 1; i <= n; ++i){
-    for(int j = i + 1; j <= n; ++j){
+  for (int i = 1; i <= n; ++i) {
+    for (int j = i + 1; j <= n; ++j) {
       ans += ProbabilityOfInversePair(a[i], a[j]);
     }
   }
+  ans = round(ans / PRECISION) * PRECISION;
 
   printf("%lf\n", ans);
 
