fd0af4b0ed1f05aba8910587c66fc093092ba319
Manvendra Kumar <manvendra.kumar7@gmail.com>
2023-05-12 15:00:58 +0530

    cses

--- cses/Tree Algorithms/fixed_length_paths_1.cpp
+++ cses/Tree Algorithms/fixed_length_paths_1.cpp
@@ -8,96 +8,81 @@ typedef long long ll;
 #define pll pair<ll, ll>
 #define N 200001
 
-vector<vector<int>> adj(N);
-int subtreeSize[N];
-bool isCentroid[N];
-vector<int> T, a;
+vector<vector<int>> adj(N), subtrees(N);
+int subtree_size[N], T[N];
 
 void Update(int i, int x) {
-  for (; i < T.size(); i += (i & -i)) {
+  for (; i < N; i += (i & -i)) {
     T[i] += x;
   }
 }
 
 int Query(int i) {
   int ans = 0;
-  for (i = min((int)T.size() - 1, max(0, i)); i > 0; i -= (i & -i)) {
+  for (i = min(N - 1, max(0, i)); i > 0; i -= (i & -i)) {
     ans += T[i];
   }
   return ans;
 }
 
-int SubtreeSizeDFS(int i, int p) {
+void SubtreeSizeDFS(int i, int p) {
   int ans = 1;
 
-  for (int j : adj[i]) {
-    if (isCentroid[j] || j == p) {
-      continue;
-    }
-    ans += SubtreeSizeDFS(j, i);
-  }
-
-  subtreeSize[i] = ans;
-  return ans;
-}
-
-int FindCentroidDFS(int i, int p, int s) {
-  for (int j : adj[i]) {
-    if (!isCentroid[j] && j != p && 2 * subtreeSize[j] > s) {
-      return FindCentroidDFS(j, i, s);
-    }
-  }
-  return i;
-}
-
-void FillSubtreeDFS(int i, int p, int d) {
-  if (isCentroid[i]) {
-    return;
-  }
-
-  a.push_back(d);
   for (int j : adj[i]) {
     if (j == p) {
       continue;
     }
-    FillSubtreeDFS(j, i, d + 1);
+    SubtreeSizeDFS(j, i);
+    ans += subtree_size[j];
   }
+
+  subtree_size[i] = ans;
 }
 
-ll ComputeAns(int k1, int k2, int i) {
-  ll ans = 0;
+ll ComputeAnsDFS(int k1, int k2, int i, int p, int d) {
+  int heavy_child = 0;
   for (int j : adj[i]) {
-    a.clear();
-    FillSubtreeDFS(j, i, 1);
-    for (int d : a) {
-      ans += Query(k2 - d) - Query(k1 - d - 1);
-    }
-
-    for (int d : a) {
-      Update(d, 1);
-    }
-  }
-  ans += Query(k2) - Query(k1 - 1);
-  T.clear();
-  return ans;
-}
-
-ll CentroidDecompositionDFS(int k1, int k2, int i) {
-  SubtreeSizeDFS(i, 0);
-
-  i = FindCentroidDFS(i, 0, subtreeSize[i]);
-  isCentroid[i] = true;
-
-  T.resize(subtreeSize[i]);
-  ll ans = ComputeAns(k1, k2, i);
-  for (int j : adj[i]) {
-    if (isCentroid[j]) {
+    if (j == p || subtree_size[j] <= subtree_size[heavy_child]) {
       continue;
     }
-    ans += CentroidDecompositionDFS(k1, k2, j);
+    heavy_child = j;
   }
 
-  isCentroid[i] = false;
+  ll ans = 0;
+  for (int j : adj[i]) {
+    if (j == p || j == heavy_child) {
+      continue;
+    }
+    ans += ComputeAnsDFS(k1, k2, j, i, d + 1);
+    for (int x : subtrees[j]) {
+      Update(x, -1);
+    }
+  }
+
+  subtrees[i].push_back(d);
+
+  if (heavy_child != 0) {
+    ans += ComputeAnsDFS(k1, k2, heavy_child, i, d + 1);
+    swap(subtrees[i], subtrees[heavy_child]);
+  } else {
+    Update(d, 1);
+  }
+
+  for (int j : adj[i]) {
+    if (j == p) {
+      continue;
+    }
+
+    for (int x : subtrees[j]) {
+      ans += Query(k2 + 2 * d - x) - Query(k1 - 1 + 2 * d - x);
+    }
+
+    for (int x : subtrees[j]) {
+      Update(x, 1);
+      subtrees[i].push_back(x);
+    }
+  }
+
   return ans;
 }
 
@@ -105,14 +90,16 @@ int main() {
   int n, k;
   scanf("%d%d", &n, &k);
 
-  for (int x = 1; x < n; ++x) {
+  for (int e = 1; e < n; ++e) {
     int i, j;
     scanf("%d%d", &i, &j);
     adj[i].push_back(j);
     adj[j].push_back(i);
   }
 
-  printf("%lld\n", CentroidDecompositionDFS(k, k, 1));
+  SubtreeSizeDFS(1, 0);
+
+  printf("%lld\n", ComputeAnsDFS(k, k, 1, 0, 1));
 
   return 0;
 }
