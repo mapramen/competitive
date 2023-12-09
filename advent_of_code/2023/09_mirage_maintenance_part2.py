4f8e0cb78ead696e37a3876dc75202d825267053
Manvendra Kumar <manvendra.kumar7@gmail.com>
2023-12-09 16:27:25 +0530

    advent of code 2023 - day 09

---
+++ advent_of_code/2023/09_mirage_maintenance_part2.py
@@ -0,0 +1,21 @@
+import sys
+
+def predict_previous_in_sequence(a):
+	diffs = [a[::-1]]
+	
+	while any(diffs[-1]):
+		diffs.append([diffs[-1][i + 1] - diffs[-1][i] for i in range(len(diffs[-1])-1)])
+	
+	diffs[-1].append(0)
+
+	for n in range(len(diffs) - 2, -1, -1):
+		diffs[n].append(diffs[n][-1] + diffs[n + 1][-1])
+
+	return diffs[0][-1]
+
+if __name__ == '__main__':
+	ans = 0
+	for line in sys.stdin:
+		a = list(map(int, line.split()))
+		ans += predict_previous_in_sequence(a)
+	print(ans)
