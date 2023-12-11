8d054e7c184fec74a35d208537f3fb710d062850
Manvendra Kumar <manvendra.kumar7@gmail.com>
2023-12-11 13:29:17 +0530

    advent of code 2023 - day 11

---
+++ advent_of_code/2023/11_cosmic_expansion_part1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+int CalculateDistance(vector<int>& a) {
+  int ans = 0;
+  for (int n = a.size(), x = 0, cnt = 0, dis = 0; x < n; dis += cnt, ++x) {
+    if (a[x] == 0) {
+      dis += cnt;
+    }
+    ans += a[x] * dis;
+    cnt += a[x];
+  }
+  return ans;
+}
+
+int main() {
+  vector<string> s;
+
+  while (true) {
+    s.push_back("");
+    if (!getline(cin, s.back())) {
+      s.pop_back();
+      break;
+    }
+  }
+
+  int n = s.size(), m = s.front().size();
+  vector<int> rows(n), columns(m);
+
+  for (int x = 0; x < n; ++x) {
+    for (int y = 0; y < m; ++y) {
+      if (s[x][y] == '#') {
+        ++rows[x];
+        ++columns[y];
+      }
+    }
+  }
+
+  int ans = CalculateDistance(rows) + CalculateDistance(columns);
+
+  cout << ans << endl;
+
+  return 0;
+}
