25d6871d4832545127769e92fc2c5e60b5f76eb1
Manvendra <manvendra.kumar7@gmail.com>
2024-05-29 17:34:43 +0530

    project euler

--- project_euler/pandigital_fibonacci_ends.py
+++ project_euler/pandigital_fibonacci_ends.py
@@ -1,5 +1,4 @@
-from math import log, log10, sqrt
-
+from math import log10, sqrt
 
 MOD = 10**9
 
@@ -7,7 +6,7 @@ def is_pan_digital(s):
 	return len(s) == 9 and set(s) == set('123456789')
 
 log_phi = log10((1 + sqrt(5)) / 2)
-log_sqrt_2 = log10(2) / 2
+log_sqrt_5 = log10(5) / 2
 
 a, b = 0, 1
 
@@ -15,12 +14,11 @@ n = 0
 while True:
 	n += 1
 	a, b = (a + b) % MOD, a
-	is_end_pan_digital = is_pan_digital(str(a))
 
-	if not is_end_pan_digital:
+	if not is_pan_digital(str(a)):
 		continue
 
-	log_fn = n * log_phi + log_sqrt_2 - 0.5
+	log_fn = n * log_phi - log_sqrt_5
 	first_9_digits = int(10 ** (log_fn - int(log_fn) + 8))
 
 	if is_pan_digital(str(first_9_digits)):
