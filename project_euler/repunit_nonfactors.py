242b492371d3e7329663e5d0a9eef4c9c1213f17
Manvendra <manvendra.kumar7@gmail.com>
2024-08-24 21:13:02 +0530

    project euler #133

---
+++ project_euler/repunit_nonfactors.py
@@ -0,0 +1,156 @@
+from math import gcd, sqrt
+from random import randint
+
+def is_prime(n):
+	if n < 2:
+		return False
+
+	if n == 2 or n == 3:
+		return True
+
+	if n % 2 == 0:
+		return False
+
+	r, d = 0, n - 1
+	while d % 2 == 0:
+		r += 1
+		d = d // 2
+
+	bases = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+	
+	for a in bases:
+		if a >= n:
+			break
+
+		x = pow(a, d, n)
+		
+		for _ in range(r - 1):
+			x = (x * x) % n
+		
+		if x == 1 or x == n - 1:
+			continue
+		
+		return False
+
+	return True
+
+def is_square(n):
+	if n < 0:
+		return False
+	
+	if n == 0 or n == 1:
+		return True
+	
+	s = int(sqrt(n))
+	return s * s == n
+
+def rho_brent(n):
+	r = randint(1, n - 1)
+	f = lambda z : (z * z + r) % n
+	
+	x = randint(1, n)
+	g = 1
+	q = 1
+	xs = x
+	y = x
+
+	m = 128
+	l = 1
+
+	while g == 1:
+		y = x
+		for _ in range(1, l):
+			x = f(x)
+		k = 0
+		while k < l and g == 1:
+			xs = x
+			for _ in range(1, min(m, l - k)):
+				x = f(x)
+				q = (q * abs(y - x)) % n
+			g = gcd(q, n)
+			k = k + m
+		l = l * 2
+
+	if g == n:
+		while True:
+			xs = f(xs)
+			g = gcd(abs(xs - y), n)
+			if g > 1:
+				break
+
+	return g
+
+def calculate_prime_factors(n):
+	if n == 1:
+		return []
+	
+	if is_square(n):
+		return calculate_prime_factors(int(sqrt(n)))
+	
+	if is_prime(n):
+		return [n]
+	
+	g = 1
+	while g == 1 or g == n:
+		g = rho_brent(n)
+
+	prime_factors = []
+	for p in calculate_prime_factors(g):
+		k = 0
+		while n % p == 0:
+			n = n // p
+			k += 1
+		prime_factors.append(p)
+
+	return prime_factors + calculate_prime_factors(n)
+
+def calculate_prime_factorization(n):
+	if n == 1:
+		return []
+	
+	prime_factorization = []
+	for p in calculate_prime_factors(n):
+		k = 0
+		while n % p == 0:
+			n = n // p
+			k += 1
+		prime_factorization.append((p, k))
+	return prime_factorization
+
+def calculate_phi(n):
+	phi = n
+	for p, _ in calculate_prime_factorization(n):
+		phi = phi // p
+		phi *= (p - 1)
+	return phi
+
+def calculate_divisors(n):
+	divisors = [1]
+	for p, k in calculate_prime_factorization(n):
+		new_divisors = []
+		for x in divisors:
+			for i in range(k + 1):
+				new_divisors.append(x * (p ** i))
+		divisors = new_divisors
+	divisors.sort()
+	return divisors
+
+def can_be_promoted_to_power_of_10(n):
+	while n > 1:
+		g = gcd(n, 10)
+		if g == 1:
+			break
+		n //= g
+	return n == 1
+
+def calculate_repunit_length(n):
+	for length in calculate_divisors(calculate_phi(9 * n)):
+		if pow(10, length, 9 * n) == 1:
+			return length
+	return n
+
+def solve(limit):
+	return sum(p for p in range(2, limit) if is_prime(p) and (p == 2 or p == 5 or not can_be_promoted_to_power_of_10(calculate_repunit_length(p))))
+
+print(100, solve(100))
+print(10**5, solve(10**5))
