7a82e7dbd2cc2a99e7e5feec2e446745125fddce
Manvendra <manvendra.kumar7@gmail.com>
2024-09-07 22:52:52 +0530

    project euler #169

--- project_euler/169.py
+++ project_euler/169.py
@@ -1,34 +1,15 @@
 import functools
 
 @functools.cache
-def solve(a):
-	if a == ():
-		return 1
-	
-	if a[0] == 0:
-		return solve(a[1:])
-	
-	if a[0] > 3:
-		return 0
-	
-	ans = 0
-	for x in range(3):
-		if x > a[0]:
-			break
-		b = list(a[1:])
-		if b:
-			b[0] += 2 * (a[0] - x)
-		if not b and x != a[0]:
-			continue
-		ans += solve(tuple(b))
-	return ans
+def stern_diatomic(n):
+	if n < 2:
+		return n
+	if n % 2 == 0:
+		return stern_diatomic(n // 2)
+	return stern_diatomic(n // 2) + stern_diatomic(n // 2 + 1)
 
 def f(n):
-	a = []
-	while n:
-		a.append(n % 2)
-		n //= 2
-	return solve(tuple(a[::-1]))
+	return stern_diatomic(n + 1)
 
 for l in range(26):
 	print(f'10^{l}: {f(10 ** l)}')
