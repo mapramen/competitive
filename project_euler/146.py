e531fc55c1e77acfef867b346f483ff576c9bad5
Manvendra <manvendra.kumar7@gmail.com>
2024-09-07 20:16:16 +0530

    project euler #146

---
+++ project_euler/146.py
@@ -0,0 +1,52 @@
+def is_prime(n):
+	if n < 2:
+		return False
+
+	if n == 2 or n == 3:
+		return True
+
+	if n % 2 == 0:
+		return False
+
+	r, d = 0, n - 1
+	while d % 2 == 0:
+		r += 1
+		d = d // 2
+
+	bases = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+	
+	for a in bases:
+		if a >= n:
+			break
+
+		x = pow(a, d, n)
+		
+		for _ in range(r - 1):
+			x = (x * x) % n
+		
+		if x == 1 or x == n - 1:
+			continue
+		
+		return False
+
+	return True
+
+present_list = [1, 3, 7, 9, 13, 27]
+
+def check(n):
+	n = n ** 2
+	for c in range(1, 28):
+		n += 1
+		valid = is_prime(n)
+		if valid and c not in present_list or not valid and c in present_list:
+			return False
+	return True
+
+ans = 0
+for n in range(0, 150 * 10 ** 6, 10):
+	if n % 3 == 0 or n % 7 == 0 or n % 13 == 0 or n % 27 == 0:
+		continue
+
+	if check(n):
+		ans += n
+		print(n, ans)
