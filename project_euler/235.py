995d8fba2b5b21ab71a250a251702c2e7265f7ba
Manvendra <manvendra.kumar7@gmail.com>
2024-09-08 18:02:50 +0530

    project euler #235

---
+++ project_euler/235.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+def agp(n, a, b, r):
+	return a * (r ** n - 1) / (r - 1) + b * (n * r ** (n + 1) - (n + 1) * r ** n + 1) / (r - 1) ** 2
+
+n, a, b = 5000, 900, -3
+target = -600000000000
+
+lower, upper = 1, 1.125
+while upper - lower > 1e-12:
+	mid = (lower + upper) / 2
+	calculated_value = agp(n, a, b, mid)
+
+	print(f"lower = {lower}, upper = {upper}, mid = {mid}, calculated_value = {calculated_value}")
+	
+	if calculated_value > target:
+		lower = mid
+	else:
+		upper = mid
+
+print(f"Answer is {(lower + upper) / 2:.12f}")
