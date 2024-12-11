b8f9d2bb0056cf3031eb0ad9da560897050f05d4
Manvendra <manvendra.kumar7@gmail.com>
2024-12-11 17:49:49 +0530

    Advent of Code 2024 - Day 11 part 2

---
+++ advent_of_code/2024/11_plutonian_pebbles_part2.py
@@ -0,0 +1,21 @@
+import functools
+
+@functools.cache
+def solve(k, n):
+	if k == 0:
+		return 1
+	
+	if n == 0:
+		return solve(k - 1, 1)
+
+	s = str(n)
+	l = len(s)
+	
+	if l % 2 == 0:
+		return solve(k - 1, int(s[:l // 2])) + solve(k - 1, int(s[l // 2:]))
+	
+	return solve(k - 1, 2024 * n)
+
+a = map(int, input().split())
+ans = sum(solve(75, n) for n in a)
+print(ans)
