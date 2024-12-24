1c01a1a462ae23e72dd750c491017e3321dbb75e
Manvendra <manvendra.kumar7@gmail.com>
2024-12-24 13:04:57 +0530

    Advent of Code 2024 - Day 24 part 1

--- advent_of_code/2024/24_crossed_wires_part1.cpp
+++ advent_of_code/2024/24_crossed_wires_part1.cpp
@@ -22,26 +22,33 @@ unordered_map<string, int> ReadInputWires() {
 	return wires;
 }
 
-unordered_map<string, tuple<string, string, string>> ReadGates() {
-	unordered_map<string, tuple<string, string, string>> gates;
+vector<tuple<string, string, string, string>> ReadGates() {
+	vector<tuple<string, string, string, string>> gates;
 	for (string line; getline(cin, line);) {
 		istringstream iss(line);
 		string input1, operation, input2, arrow, output;
 		iss >> input1 >> operation >> input2 >> arrow >> output;
 
-		gates[output] = make_tuple(operation, input1, input2);
+		if (input1 > input2) {
+			swap(input1, input2);
+		}
+
+		gates.push_back({input1, input2, operation, output});
 	}
+	sort(gates.begin(), gates.end());
 	return gates;
 }
 
 int main() {
 	unordered_map<string, int> gate_values = ReadInputWires();
-	unordered_map<string, tuple<string, string, string>> gates = ReadGates();
+	vector<tuple<string, string, string, string>> gates = ReadGates();
+
+	// for (auto [input1, input2, operation, output] : gates) {
+	// 	cout << input1 << " " << operation << " " << input2 << " -> " << output << endl;
+	// }
 
 	for (int k = 0; k < gates.size(); ++k) {
-		for (const auto [output, gate] : gates) {
-			const auto [operation, input1, input2] = gate;
-
+		for (const auto [input1, input2, operation, output] : gates) {
 			int input1_value = gate_values.count(input1) ? gate_values[input1] : 0;
 			int input2_value = gate_values.count(input2) ? gate_values[input2] : 0;
 
