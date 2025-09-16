5121443413be53a0761c5c27699794db24a695cf
Manvendra <manvendra.kumar7@gmail.com>
2025-09-17 00:03:03 +0530

    Kick Start 2022 - Round G - Cute Little Butterfly

--- kickstart/2022/round_g/cute_little_butterfly.cpp
+++ kickstart/2022/round_g/cute_little_butterfly.cpp
@@ -6,90 +6,47 @@ typedef long long ll;
 
 #define pii pair<int, int>
 #define pll pair<ll, ll>
-#define N 100000
 
-struct NodeData {
-	ll lazy_update;
-	ll ans;
-};
-
-NodeData dp_left[4 * N], dp_right[4 * N];
-
-void Reset(NodeData T[], const int k, const int l, const int r) {
-	T[k].lazy_update = LLONG_MIN;
-	T[k].ans = LLONG_MIN;
-
-	if (l == r) {
-		return;
+void UpdateLeft(map<int, ll>& dp, const int x, const ll val) {
+	while (!dp.empty() && dp.begin()->first < x) {
+		const auto it = prev(dp.lower_bound(x));
+		if (it->second > val) {
+			break;
+		}
+		dp.erase(it);
 	}
 
-	const int mid = (l + r) / 2;
-	Reset(T, 2 * k + 1, l, mid);
-	Reset(T, 2 * k + 2, mid + 1, r);
+	auto it = dp.find(x);
+	if (it != dp.end()) {
+		it->second = max(it->second, val);
+	} else {
+		dp[x] = val;
+	}
 }
 
-void Reset() {
-	Reset(dp_left, 0, 0, N);
-	Reset(dp_right, 0, 0, N);
+ll QueryLeft(map<int, ll>& dp, const int x) {
+	return dp.empty() || x > dp.rbegin()->first ? LLONG_MIN : dp.lower_bound(x)->second;
 }
 
-void UpdateNode(NodeData T[], const int k, const ll x) {
-	if (x <= T[k].lazy_update) {
-		return;
+void UpdateRight(map<int, ll>& dp, const int x, const ll val) {
+	while (!dp.empty() && dp.rbegin()->first > x) {
+		const auto it = dp.upper_bound(x);
+		if (it->second > val) {
+			break;
+		}
+		dp.erase(it);
 	}
 
-	T[k].lazy_update = x;
-	T[k].ans = max(T[k].ans, x);
-}
-
-void LazyUpdateChildren(NodeData T[], const int k) {
-	UpdateNode(T, 2 * k + 1, T[k].lazy_update);
-	UpdateNode(T, 2 * k + 2, T[k].lazy_update);
-}
-
-void Combine(NodeData T[], const int k) {
-	T[k].ans = max(T[2 * k + 1].ans, T[2 * k + 2].ans);
-}
-
-void Update(NodeData T[], const int k, const int l, const int r, const int ql, const int qr, const ll x) {
-	if (r < l || qr < l || r < ql) {
-		return;
+	auto it = dp.find(x);
+	if (it != dp.end()) {
+		it->second = max(it->second, val);
+	} else {
+		dp[x] = val;
 	}
-
-	if (ql <= l && r <= qr) {
-		UpdateNode(T, k, x);
-		return;
-	}
-
-	LazyUpdateChildren(T, k);
-
-	const int mid = (l + r) / 2;
-	Update(T, 2 * k + 1, l, mid, ql, qr, x);
-	Update(T, 2 * k + 2, mid + 1, r, ql, qr, x);
-	Combine(T, k);
 }
 
-void Update(NodeData T[], const int ql, const int qr, const ll x) {
-	return Update(T, 0, 0, N, ql, qr, x);
-}
-
-ll Query(NodeData T[], const int k, const int l, const int r, const int i) {
-	if (r < l || i < l || r < i) {
-		return LLONG_MIN;
-	}
-
-	if (i == l && r == i) {
-		return T[k].ans;
-	}
-
-	LazyUpdateChildren(T, k);
-
-	const int mid = (l + r) / 2;
-	return i <= mid ? Query(T, 2 * k + 1, l, mid, i) : Query(T, 2 * k + 2, mid + 1, r, i);
-}
-
-ll Query(NodeData T[], const int i) {
-	return Query(T, 0, 0, N, i);
+ll QueryRight(map<int, ll>& dp, const int x) {
+	return dp.empty() || x < dp.begin()->first ? LLONG_MIN : prev(dp.upper_bound(x))->second;
 }
 
 ll Solve() {
@@ -97,31 +54,34 @@ ll Solve() {
 	scanf("%d%d", &n, &e);
 
 	map<int, vector<pair<int, int>>> y_to_flowers;
+	int xmin = INT_MAX, xmax = INT_MIN;
 	while (n--) {
 		int x, y, c;
 		scanf("%d%d%d", &x, &y, &c);
 		y_to_flowers[-y].push_back({x, c});
+		xmin = min(xmin, x);
+		xmax = max(xmax, x);
 	}
 
-	Reset();
-	Update(dp_right, 0, N, 0);
+	map<int, ll> dp_left, dp_right;
+	UpdateRight(dp_right, xmin, 0);
 
 	for (auto [_, v] : y_to_flowers) {
-		Update(dp_left, 0, N, Query(dp_right, N) - e);
-		Update(dp_right, 0, N, Query(dp_left, 0) - e);
+		UpdateLeft(dp_left, xmax, QueryRight(dp_right, xmax) - e);
+		UpdateRight(dp_right, xmin, QueryLeft(dp_left, xmin) - e);
 
 		sort(v.begin(), v.end());
 		for (const auto [x, c] : v) {
-			Update(dp_right, x, N, Query(dp_right, x) + c);
+			UpdateRight(dp_right, x, QueryRight(dp_right, x) + c);
 		}
 
 		reverse(v.begin(), v.end());
 		for (const auto [x, c] : v) {
-			Update(dp_left, 0, x, Query(dp_left, x) + c);
+			UpdateLeft(dp_left, x, QueryLeft(dp_left, x) + c);
 		}
 	}
 
-	return max(Query(dp_left, 0), Query(dp_right, N));
+	return max(QueryLeft(dp_left, xmin), QueryRight(dp_right, xmax));
 }
 
 int main() {
