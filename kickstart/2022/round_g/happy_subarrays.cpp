63e5889997ecc19bbf52aa7ca65a6e38bfa60276
Manvendra <manvendra.kumar7@gmail.com>
2025-09-17 13:36:31 +0530

    Kick Start 2022 - Round G - Happy Subarrays

--- kickstart/2022/round_g/happy_subarrays.cpp
+++ kickstart/2022/round_g/happy_subarrays.cpp
@@ -7,14 +7,6 @@ typedef long long ll;
 #define pii pair<int, int>
 #define pll pair<ll, ll>
 
-int SearchRightEnd(const int n, const vector<pair<int, int>>& v, const int x) {
-	if (x <= v.front().first) {
-		return n;
-	}
-
-	return prev(lower_bound(v.begin(), v.end(), make_pair(x, INT_MIN)))->second - 1;
-}
-
 ll Solve() {
 	int n;
 	scanf("%d", &n);
@@ -32,20 +24,17 @@ ll Solve() {
 	}
 
 	ll ans = 0;
-	vector<pair<int, int>> v;
+	vector<int> S{n};
 	for (int i = n; i > 0; --i) {
-		const int s = prefix_sums[i];
-		while (!v.empty() && s <= v.back().first) {
-			v.pop_back();
-		}
-		v.push_back({s, i});
-
-		const int j = SearchRightEnd(n, v, prefix_sums[i - 1]);
-		if (j < i) {
-			continue;
+		const int s = prefix_sums[i - 1];
+		while (!S.empty() && s <= prefix_sums[S.back()]) {
+			S.pop_back();
 		}
 
+		const int j = S.empty() ? n : S.back() - 1;
 		ans += (sums_of_sums[j] - sums_of_sums[i - 1]) - 1ll * (j - i + 1) * prefix_sums[i - 1];
+
+		S.push_back(i - 1);
 	}
 
 	return ans;
