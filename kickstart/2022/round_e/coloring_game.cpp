0b09b67171e9bf5e2169ffb022e8b51d41ca23e7
Manvendra <manvendra.kumar7@gmail.com>
2025-09-20 23:11:22 +0530

    Kick Start 2022 - Round E - Coloring Game

---
+++ kickstart/2022/round_e/coloring_game.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+int Solve() {
+	int n;
+	scanf("%d", &n);
+	return (n + 4) / 5;
+}
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	for (int k = 1; k <= t; ++k) {
+		printf("Case #%d: %d\n", k, Solve());
+	}
+	return 0;
+}
