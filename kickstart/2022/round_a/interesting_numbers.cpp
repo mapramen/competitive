29aa72f98a612803a920fb57c68c513d1caa950c
Manvendra <manvendra.kumar7@gmail.com>
2025-09-25 19:26:36 +0530

    Kick Start 2022 - Round A - Interesting Integers

--- kickstart/2022/round_a/interesting_numbers.cpp
+++ kickstart/2022/round_a/interesting_numbers.cpp
@@ -25,8 +25,17 @@ int CalculateMaxPossibleProduct(const int sum) {
 	return CalculateMaxPossibleProduct(sum, 2) * CalculateMaxPossibleProduct(sum, 3) * CalculateMaxPossibleProduct(sum, 5) * CalculateMaxPossibleProduct(sum, 7);
 }
 
-int CalculateStateProduct(const int max_product, const int p) {
-	return p == 0 ? 0 : __gcd(max_product, p);
+int CalculateNewProduct(const int P, const int p, const int d) {
+	const int new_product = p * d;
+	return new_product == 0 ? 0 : __gcd(P, new_product);
+}
+
+int CalculateNewSum(const int s, const int d) {
+	return s + d;
+}
+
+int CalculateNewState(const int P, const int S, const int p, const int s, const int d) {
+	return CalculateNewProduct(P, p, d) * S + CalculateNewSum(s, d);
 }
 
 bool IsValid(const ll p, const int s) {
@@ -39,44 +48,44 @@ ll Calculate(const ll n) {
 	}
 
 	const auto& v = Int2Vec(n);
-	const int max_sum = v[0] + (v.size() - 1) * 9;
-	const int max_product = CalculateMaxPossibleProduct(max_sum);
+	const int S = v[0] + (v.size() - 1) * 9 + 1;
+	const int P = CalculateMaxPossibleProduct(S - 1);
 
-	map<pair<int, int>, ll> dp_cur, dp_next;
+	unordered_map<int, ll> dp_cur, dp_next;
 
 	for (int d = 1; d < v[0]; ++d) {
-		++dp_cur[{CalculateStateProduct(max_product, d), d}];
+		++dp_cur[CalculateNewState(P, S, 1, 0, d)];
 	}
 
-	int running_product = CalculateStateProduct(max_product, v[0]), running_sum = v[0];
+	int running_product = CalculateNewProduct(P, 1, v[0]), running_sum = CalculateNewSum(0, v[0]);
 
 	for (int i = 1; i < v.size(); ++i) {
 		dp_next.clear();
 
 		for (const auto [k, cnt] : dp_cur) {
-			const auto [p, s] = k;
+			const int p = k / S, s = k % S;
 			for (int d = 0; d < B; ++d) {
-				dp_next[{CalculateStateProduct(max_product, p * d), s + d}] += cnt;
+				dp_next[CalculateNewState(P, S, p, s, d)] += cnt;
 			}
 		}
 
 		for (int d = 1; d < B; ++d) {
-			++dp_next[{CalculateStateProduct(max_product, d), d}];
+			++dp_next[CalculateNewState(P, S, 1, 0, d)];
 		}
 
 		for (int d = 0; d < v[i]; ++d) {
-			++dp_next[{CalculateStateProduct(max_product, running_product * d), running_sum + d}];
+			++dp_next[CalculateNewState(P, S, running_product, running_sum, d)];
 		}
 
-		running_product = CalculateStateProduct(max_product, running_product * v[i]);
-		running_sum += v[i];
+		running_product = CalculateNewProduct(P, running_product, v[i]);
+		running_sum = CalculateNewSum(running_sum, v[i]);
 
 		dp_cur.swap(dp_next);
 	}
 
 	ll ans = IsValid(running_product, running_sum);
 	for (const auto [k, cnt] : dp_cur) {
-		const auto [p, s] = k;
+		const int p = k / S, s = k % S;
 		if (IsValid(p, s)) {
 			ans += cnt;
 		}
