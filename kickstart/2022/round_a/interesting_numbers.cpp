58ed18484cf4ee903a83d83c5d81fec53e9aceac
Manvendra <manvendra.kumar7@gmail.com>
2025-09-25 19:14:36 +0530

    Kick Start 2022 - Round A - Interesting Integers

---
+++ kickstart/2022/round_a/interesting_numbers.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define B 10
+
+vector<int> Int2Vec(ll n) {
+	vector<int> v;
+	for (; n > 0; n /= 10) {
+		v.push_back(n % 10);
+	}
+	reverse(v.begin(), v.end());
+	return v;
+}
+
+int CalculateMaxPossibleProduct(const int sum, const int p) {
+	return sum < p ? 1 : p * CalculateMaxPossibleProduct(sum / p, p);
+}
+
+int CalculateMaxPossibleProduct(const int sum) {
+	return CalculateMaxPossibleProduct(sum, 2) * CalculateMaxPossibleProduct(sum, 3) * CalculateMaxPossibleProduct(sum, 5) * CalculateMaxPossibleProduct(sum, 7);
+}
+
+int CalculateStateProduct(const int max_product, const int p) {
+	return p == 0 ? 0 : __gcd(max_product, p);
+}
+
+bool IsValid(const ll p, const int s) {
+	return p % s == 0;
+}
+
+ll Calculate(const ll n) {
+	if (n == 0) {
+		return 0ll;
+	}
+
+	const auto& v = Int2Vec(n);
+	const int max_sum = v[0] + (v.size() - 1) * 9;
+	const int max_product = CalculateMaxPossibleProduct(max_sum);
+
+	map<pair<int, int>, ll> dp_cur, dp_next;
+
+	for (int d = 1; d < v[0]; ++d) {
+		++dp_cur[{CalculateStateProduct(max_product, d), d}];
+	}
+
+	int running_product = CalculateStateProduct(max_product, v[0]), running_sum = v[0];
+
+	for (int i = 1; i < v.size(); ++i) {
+		dp_next.clear();
+
+		for (const auto [k, cnt] : dp_cur) {
+			const auto [p, s] = k;
+			for (int d = 0; d < B; ++d) {
+				dp_next[{CalculateStateProduct(max_product, p * d), s + d}] += cnt;
+			}
+		}
+
+		for (int d = 1; d < B; ++d) {
+			++dp_next[{CalculateStateProduct(max_product, d), d}];
+		}
+
+		for (int d = 0; d < v[i]; ++d) {
+			++dp_next[{CalculateStateProduct(max_product, running_product * d), running_sum + d}];
+		}
+
+		running_product = CalculateStateProduct(max_product, running_product * v[i]);
+		running_sum += v[i];
+
+		dp_cur.swap(dp_next);
+	}
+
+	ll ans = IsValid(running_product, running_sum);
+	for (const auto [k, cnt] : dp_cur) {
+		const auto [p, s] = k;
+		if (IsValid(p, s)) {
+			ans += cnt;
+		}
+	}
+	return ans;
+}
+
+ll Solve() {
+	ll l, r;
+	scanf("%lld%lld", &l, &r);
+	return Calculate(r) - Calculate(l - 1);
+}
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	for (int k = 1; k <= t; ++k) {
+		printf("Case #%d: %lld\n", k, Solve());
+	}
+	return 0;
+}
