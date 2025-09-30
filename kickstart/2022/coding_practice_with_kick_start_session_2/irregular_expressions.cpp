080a1b62f57df1f3d1db574716d89896a36a4bc8
Manvendra <manvendra.kumar7@gmail.com>
2025-09-30 22:34:06 +0530

    Kick Start 2022 - Coding Practice with Kick Start Session #2 - Irregular Expressions

--- kickstart/2022/coding_practice_with_kick_start_session_2/irregular_expressions.cpp
+++ kickstart/2022/coding_practice_with_kick_start_session_2/irregular_expressions.cpp
@@ -11,28 +11,25 @@ bool IsVowel(const char c) {
 	return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
 }
 
+int GetNextVowelIndex(const string& s, int i) {
+	const int n = s.size();
+	for (++i; i < n && !IsVowel(s[i]); ++i);
+	return i;
+}
+
 bool Solve() {
 	string s;
 	cin >> s;
 
 	const int n = s.size();
-	for (int x = 0; x < n; ++x) {
-		int cnt1 = 0;
-		for (int y = x; y < n; ++y) {
-			if (cnt1 >= 2) {
-				int cnt2 = 0;
-				for (int z = y; z + (y - x) <= n; ++z) {
-					if (cnt2 != 0) {
-						int i = x, j = z;
-						for (; i < y && s[i] == s[j]; ++i, ++j);
-						if (i == y) {
-							return true;
-						}
-					}
-					cnt2 += IsVowel(s[z]);
-				}
+	for (int x = GetNextVowelIndex(s, -1); x < n; x = GetNextVowelIndex(s, x)) {
+		const int y = GetNextVowelIndex(s, x);
+		for (int z = GetNextVowelIndex(s, GetNextVowelIndex(s, y)); z + (y - x) < n; z = GetNextVowelIndex(s, z)) {
+			int i = x, j = z;
+			for (; i <= y && s[i] == s[j]; ++i, ++j);
+			if (i > y) {
+				return true;
 			}
-			cnt1 += IsVowel(s[y]);
 		}
 	}
 
