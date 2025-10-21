4bc7c997648bd5374b51742e56bc65d28efdbf87
Manvendra <manvendra.kumar7@gmail.com>
2025-10-21 13:55:59 +0530

    Kick Start 2021 - Round H - Dependent Events

--- kickstart/2021/round_h/dependent_events.cpp
+++ kickstart/2021/round_h/dependent_events.cpp
@@ -11,45 +11,53 @@ typedef long long ll;
 #define MOD_INV 142857001
 
 int d[N], dp[N];
-vector<vector<pair<int, vector<vector<int>>>>> T(LOGN, vector<pair<int, vector<vector<int>>>>(N));
+int ancestor[LOGN][N];
+int prob[LOGN][N][K][K];
+int A[K][K], B[K][K], T[K][K];
 
-vector<vector<int>> GetIdentityMatrix() {
-	vector<vector<int>> A(K, vector<int>(K));
+void FillIdentityMatrix(int A[K][K]) {
 	for (int i = 0; i < K; ++i) {
+		for (int j = 0; j < K; ++j) {
+			A[i][j] = 0;
+		}
 		A[i][i] = 1;
 	}
-	return A;
 }
 
-vector<vector<int>> GetProbabilityMatrix(const int a, const int b) {
-	vector<vector<int>> A(K, vector<int>(K));
+void FillProbabilityMatrix(int A[K][K], const int a, const int b) {
 	A[0][0] = a;
 	A[0][1] = (MOD + 1 - a) % MOD;
 	A[1][0] = b;
 	A[1][1] = (MOD + 1 - b) % MOD;
-	return A;
 }
 
 void Reset(const int n) {
 	const int logn = 2 + __lg(n);
 	for (int k = 0; k < logn; ++k) {
-		T[k][0] = {0, GetIdentityMatrix()};
+		ancestor[k][0] = 0;
+		FillIdentityMatrix(prob[k][0]);
 	}
-	T[0][1] = {0, GetIdentityMatrix()};
+
+	ancestor[0][1] = 0;
+	FillIdentityMatrix(prob[0][1]);
 }
 
-vector<vector<int>> MatrixMultiply(const vector<vector<int>>& A, const vector<vector<int>>& B) {
-	vector<vector<int>> C(K, vector<int>(K));
+void MatrixMultiply(const int A[K][K], const int B[K][K], int C[K][K]) {
 	for (int i = 0; i < K; ++i) {
 		for (int j = 0; j < K; ++j) {
 			int ans = 0;
 			for (int k = 0; k < K; ++k) {
 				ans = (ans + 1ll * A[i][k] * B[k][j]) % MOD;
 			}
-			C[i][j] = ans;
+			T[i][j] = ans;
 		}
 	}
-	return C;
+
+	for (int i = 0; i < K; ++i) {
+		for (int j = 0; j < K; ++j) {
+			C[i][j] = T[i][j];
+		}
+	}
 }
 
 int Query(int i, int j) {
@@ -59,14 +67,14 @@ int Query(int i, int j) {
 
 	const int logn = 2 + __lg(d[j]);
 
-	vector<vector<int>> B = GetIdentityMatrix();
+	FillIdentityMatrix(B);
 	for (int k = logn - 1, x = d[j] - d[i]; k > -1 && x > 0; --k) {
 		if ((1 << k) > x) {
 			continue;
 		}
 
-		B = MatrixMultiply(T[k][j].second, B);
-		j = T[k][j].first;
+		MatrixMultiply(prob[k][j], B, B);
+		j = ancestor[k][j];
 		x ^= (1 << k);
 	}
 
@@ -74,23 +82,23 @@ int Query(int i, int j) {
 		return (1ll * dp[i] * B[0][0]) % MOD;
 	}
 
-	vector<vector<int>> A = GetIdentityMatrix();
+	FillIdentityMatrix(A);
 	for (int k = logn - 1; k > -1; --k) {
-		if (T[k][i].first == T[k][j].first) {
+		if (ancestor[k][i] == ancestor[k][j]) {
 			continue;
 		}
 
-		A = MatrixMultiply(T[k][i].second, A);
-		B = MatrixMultiply(T[k][j].second, B);
+		MatrixMultiply(prob[k][i], A, A);
+		MatrixMultiply(prob[k][j], B, B);
 
-		i = T[k][i].first;
-		j = T[k][j].first;
+		i = ancestor[k][i];
+		j = ancestor[k][j];
 	}
 
-	A = MatrixMultiply(T[0][i].second, A);
-	B = MatrixMultiply(T[0][j].second, B);
+	MatrixMultiply(prob[0][i], A, A);
+	MatrixMultiply(prob[0][j], B, B);
 
-	const int lca = T[0][i].first;
+	const int lca = ancestor[0][i];
 	const int p = (1ll * A[0][0] * B[0][0]) % MOD, q = (1ll * A[1][0] * B[1][0]) % MOD;
 	return (1ll * p * dp[lca] + 1ll * q * (MOD + 1 - dp[lca])) % MOD;
 }
@@ -113,14 +121,18 @@ vector<int> Solve() {
 
 		d[i] = 1 + d[p];
 		dp[i] = (1ll * x * dp[p] + 1ll * y * (MOD + 1 - dp[p])) % MOD;
-		T[0][i] = {p, GetProbabilityMatrix(x, y)};
+
+		ancestor[0][i] = p;
+		FillProbabilityMatrix(prob[0][i], x, y);
 	}
 
 	const int logn = 2 + __lg(n);
 	for (int k = 1; k < logn; ++k) {
 		for (int i = 1; i <= n; ++i) {
-			const auto [j, _] = T[k - 1][i];
-			T[k][i] = {T[k - 1][j].first, MatrixMultiply(T[k - 1][j].second, T[k - 1][i].second)};
+			const int j = ancestor[k - 1][i];
+
+			MatrixMultiply(prob[k - 1][j], prob[k - 1][i], prob[k][i]);
+			ancestor[k][i] = ancestor[k - 1][j];
 		}
 	}
 
