2f671c2debe8702c138d6d4a475c191587d0e75a
Manvendra <manvendra.kumar7@gmail.com>
2026-03-14 15:08:09 +0530

    CSES - Dynamic Programming - Minimal Grid Path

---
+++ cses/Dynamic Programming/minimal_grid_path.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void checkAndPush(vector<string>& S, string& ans, vector<pair<int, int>>& nxt_level, const int x, const int y) {
+	if (x == S.size() || y == S.size() || S[x][y] == '\0') {
+		return;
+	}
+
+	const char till_now_next_char = ans.back();
+	const char candidate = S[x][y];
+	S[x][y] = '\0';
+
+	if (till_now_next_char < candidate) {
+		return;
+	}
+
+	if (candidate < till_now_next_char) {
+		ans.pop_back();
+		ans.push_back(candidate);
+		nxt_level.clear();
+	}
+
+	nxt_level.push_back({x, y});
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	vector<string> S(n);
+	for (string& s : S) {
+		cin >> s;
+	}
+
+	string ans{'Z' + 1};
+	vector<pair<int, int>> cur_level, nxt_level;
+	checkAndPush(S, ans, cur_level, 0, 0);
+
+	while (!cur_level.empty()) {
+		ans.push_back('Z' + 1);
+
+		for (const auto [x, y] : cur_level) {
+			checkAndPush(S, ans, nxt_level, x, y + 1);
+			checkAndPush(S, ans, nxt_level, x + 1, y);
+		}
+
+		cur_level.swap(nxt_level);
+		nxt_level.clear();
+	}
+
+	ans.pop_back();
+	cout << ans << '\n';
+
+	return 0;
+}
