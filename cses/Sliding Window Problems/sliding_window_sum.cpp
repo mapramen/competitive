eab6c0eb210be5abc723faa3fef20610bcfabf22
Manvendra <manvendra.kumar7@gmail.com>
2026-03-15 14:10:34 +0530

    CSES - Sliding Window Problems - Sliding Window Sum

---
+++ cses/Sliding Window Problems/sliding_window_sum.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int GenerateNext(const int a, const int b, const int c, const int x) {
+	return (1ll * a * x + b) % c;
+}
+
+int main() {
+	int n, k;
+	scanf("%d%d", &n, &k);
+
+	int x, a, b, c;
+	scanf("%d%d%d%d", &x, &a, &b, &c);
+
+	vector<int> A(k);
+	A[0] = x;
+
+	for (int i = 1; i < k; ++i) {
+		x = GenerateNext(a, b, c, x);
+		A[i] = x;
+	}
+
+	long long sum = accumulate(A.begin(), A.end(), 0ll);
+	long long ans = sum;
+
+	for (int i = k; i < n; ++i) {
+		const int j = i % k;
+
+		sum -= A[j];
+		x = GenerateNext(a, b, c, x);
+		A[j] = x;
+
+		sum += A[j];
+		ans ^= sum;
+	}
+
+	printf("%lld\n", ans);
+	return 0;
+}
