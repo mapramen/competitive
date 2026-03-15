2ffd4b58806027f0e903ebf6d913cdf507686944
Manvendra <manvendra.kumar7@gmail.com>
2026-03-15 15:41:23 +0530

    CSES - Sliding Window Problems - Sliding Window Or

--- cses/Sliding Window Problems/sliding_window_or.cpp
+++ cses/Sliding Window Problems/sliding_window_or.cpp
@@ -8,6 +8,55 @@ int GenerateNext(const int a, const int b, const int c, const int x) {
 	return (1ll * a * x + b) % c;
 }
 
+struct AggStack {
+	// Each element is stored as (value, current_agg)
+	stack<pair<int, int>> st;
+
+	// Push a new number; compute the new agg
+	void push(int x) {
+		int cur = st.empty() ? x : st.top().second | x;
+		st.push({x, cur});
+	}
+
+	// Pop the top element.
+	void pop() {
+		st.pop();
+	}
+
+	// Return the current agg.
+	int agg() const {
+		return st.top().second;
+	}
+};
+
+struct AggQueue {
+	AggStack in, out;
+
+	// Push a new number into the queue.
+	void push(int x) {
+		in.push(x);
+	}
+
+	// Pop the oldest number.
+	void pop() {
+		if (out.st.empty()) {
+			while (!in.st.empty()) {
+				int v = in.st.top().first;
+				in.pop();
+				out.push(v);
+			}
+		}
+		out.pop();
+	}
+
+	// Query
+	int query() const {
+		if (in.st.empty()) return out.agg();
+		if (out.st.empty()) return in.agg();
+		return in.agg() | out.agg();
+	}
+};
+
 int main() {
 	int n, k;
 	scanf("%d%d", &n, &k);
@@ -15,49 +64,24 @@ int main() {
 	int x, a, b, c;
 	scanf("%d%d%d%d", &x, &a, &b, &c);
 
-	vector<pair<int, int>> S;
-	S.push_back({x, 0});
+	AggQueue Q;
+	Q.push(x);
 
 	int ans = k == 1 ? x : 0;
 
 	for (int i = 1; i < n; ++i) {
 		x = GenerateNext(a, b, c, x);
-		S.push_back({x, i});
+		Q.push(x);
 
 		if (i < k - 1) {
 			continue;
 		}
 
-		int or_sum = 0;
-		for (int j = S.size() - 1; j > -1; --j) {
-			if (S[j].second <= i - k || (S[j].first & or_sum) == S[j].first) {
-				S[j].first = 0;
-				continue;
-			}
-			or_sum |= S[j].first;
+		if (i >= k) {
+			Q.pop();
 		}
 
-		for (int p = 0, q = 0; q < S.size(); ++q) {
-			if (S[q].first == 0) {
-				continue;
-			}
-
-			while (p < q && S[p].first != 0) {
-				++p;
-			}
-
-			if (p == q) {
-				continue;
-			}
-
-			swap(S[p], S[q]);
-		}
-
-		while (!S.empty() && S.back().first == 0) {
-			S.pop_back();
-		}
-
-		ans ^= or_sum;
+		ans ^= Q.query();
 	}
 
 	printf("%d\n", ans);
