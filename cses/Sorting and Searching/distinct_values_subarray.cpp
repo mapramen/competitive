f020301b77f0bfd36c3d32f3778706676a6a580c
Manvendra <manvendra.kumar7@gmail.com>
2026-03-16 21:51:20 +0530

    CSES - Sorting and Searching - Distinct Values Subarrays

--- cses/Sorting and Searching/distinct_values_subarray.cpp
+++ cses/Sorting and Searching/distinct_values_subarray.cpp
@@ -2,6 +2,21 @@
 
 using namespace std;
 
+struct custom_hash {
+	static uint64_t splitmix64(uint64_t x) {
+		// http://xorshift.di.unimi.it/splitmix64.c
+		x += 0x9e3779b97f4a7c15;
+		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+		return x ^ (x >> 31);
+	}
+
+	size_t operator()(uint64_t x) const {
+		static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+		return splitmix64(x + FIXED_RANDOM);
+	}
+};
+
 int main() {
 	int n;
 	scanf("%d", &n);
@@ -11,7 +26,7 @@ int main() {
 		scanf("%d", &x);
 	}
 
-	set<int> S;
+	unordered_set<int, custom_hash> S;
 	long long ans = 0;
 
 	for (int l = 0, r = 0; r < n; ++r) {
