c4bb30f52a6f0c0faacba070ab7ee7c46a318799
Manvendra <manvendra.kumar7@gmail.com>
2026-03-21 12:08:04 +0530

    CSES - Counting Problems - All Letter Subgrid Count II

--- cses/Counting Problems/all_letter_subgrid_count_2.cpp
+++ cses/Counting Problems/all_letter_subgrid_count_2.cpp
@@ -2,102 +2,58 @@
 
 using namespace std;
 
-#define N 3001
+#define N 501
+#define LOGN 9
 
-char S[N][N];
-int a[N];
+char s[N];
+int T[LOGN][LOGN][N][N];
 
-struct AggStack {
-	// Each element is stored as (value, current_agg)
-	stack<pair<int, int>> st;
-
-	// Push a new number; compute the new agg
-	void push(int x) {
-		int cur = st.empty() ? x : st.top().second | x;
-		st.push({x, cur});
-	}
-
-	// Pop the top element.
-	void pop() {
-		st.pop();
-	}
-
-	// Return the current agg.
-	int agg() const {
-		return st.top().second;
-	}
-};
-
-struct AggQueue {
-	AggStack in, out;
-
-	bool empty() const {
-		return size() == 0;
-	}
-
-	size_t size() const {
-		return in.st.size() + out.st.size();
-	}
-
-	// Push a new number into the queue.
-	void push(int x) {
-		in.push(x);
-	}
-
-	// Pop the oldest number.
-	void pop() {
-		if (out.st.empty()) {
-			while (!in.st.empty()) {
-				int v = in.st.top().first;
-				in.pop();
-				out.push(v);
-			}
-		}
-		out.pop();
-	}
-
-	// Query
-	int query() const {
-		if (in.st.empty()) return out.agg();
-		if (out.st.empty()) return in.agg();
-		return in.agg() | out.agg();
-	}
-
-	void clear() {
-		while (!empty()) {
-			pop();
-		}
-	}
-};
+int Query(const int x1, const int y1, const int x2, const int y2) {
+	const int kx = __lg(x2 - x1 + 1);
+	const int ky = __lg(y2 - y1 + 1);
+	return T[kx][ky][x2][y2] | T[kx][ky][x1 + (1 << kx) - 1][y2] | T[kx][ky][x2][y1 + (1 << ky) - 1] | T[kx][ky][x1 + (1 << kx) - 1][y1 + (1 << ky) - 1];
+}
 
 int main() {
 	int n, k;
 	scanf("%d%d", &n, &k);
 
-	for (int i = 0; i < n; ++i) {
-		scanf("%s", S[i]);
-	}
+	for (int x = 1; x <= n; ++x) {
+		scanf("%s", s);
 
-	long long ans = 0;
-	AggQueue Q;
-
-	for (int x1 = 1; x1 <= n; ++x1) {
-		fill(a + 1, a + n + 1, 0);
-		for (int x2 = x1; x2 <= n; ++x2) {
-			for (int y = 1; y <= n; ++y) {
-				a[y] = a[y] | (1 << (S[x2 - 1][y - 1] - 'A'));
-				Q.push(a[y]);
-
-				while (!Q.empty() && __builtin_popcount(Q.query()) == k) {
-					Q.pop();
-				}
-
-				ans += y - Q.size();
-			}
-			Q.clear();
+		for (int y = 1; y <= n; ++y) {
+			T[0][0][x][y] = 1 << (s[y - 1] - 'A');
 		}
 	}
 
+	for (int i = 0; i < LOGN; ++i) {
+		if (i != 0) {
+			for (int x = (1 << i); x <= n; ++x) {
+				for (int y = 1; y <= n; ++y) {
+					T[i][0][x][y] = T[i - 1][0][x][y] | T[i - 1][0][x - (1 << (i - 1))][y];
+				}
+			}
+		}
+
+		for (int j = 1; j < LOGN; ++j) {
+			for (int x = 1; x <= n; ++x) {
+				for (int y = (1 << j); y <= n; ++y) {
+					T[i][j][x][y] = T[i][j - 1][x][y] | T[i][j - 1][x][y - (1 << (j - 1))];
+				}
+			}
+		}
+	}
+
+	const int all_set = (1 << k) - 1;
+	long long ans = 0;
+	for (int x1 = 1; x1 <= n; ++x1) {
+		for (int x2 = x1; x2 <= n; ++x2) {
+			for (int y1 = 1, y2 = 1; y2 <= n; ++y2) {
+				for (; y1 <= y2 && Query(x1, y1, x2, y2) == all_set; ++y1);
+				ans += y1 - 1;
+			}
+		}
+	}
 	printf("%lld\n", ans);
 
 	return 0;
