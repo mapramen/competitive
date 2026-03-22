ec2a9b1d1472bc90414ca9e0cd9f9903fd84266a
Manvendra <manvendra.kumar7@gmail.com>
2026-03-22 13:01:40 +0530

    CSES - Advanced Graph Problems - Split into Two Paths

--- cses/Advanced Graph Problems/split_into_two_paths.cpp
+++ cses/Advanced Graph Problems/split_into_two_paths.cpp
@@ -1,162 +1,133 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
-namespace IO {
-inline int readChar() { return getchar_unlocked(); }
+const int MAXN = 200005;
+const int INF = 1e9;
 
-inline int readInt() {
-	int c = readChar(), x = 0;
-	while (c <= ' ') c = readChar();
-	for (; c > ' '; c = readChar()) x = x * 10 + c - '0';
-	return x;
-}
+vector<int> adj[MAXN];
+int matchU[MAXN], matchV[MAXN], dist[MAXN];
+int in_degree[MAXN], topo[MAXN];
+int n, m;
 
-inline void writeChar(char c) { putchar_unlocked(c); }
-
-inline void writeInt(int x, char end = '\0') {
-	if (x == 0) {
-		writeChar('0');
-	} else {
-		char buf[12];
-		int len = 0;
-		for (; x > 0; x /= 10) buf[len++] = '0' + x % 10;
-		for (int i = len - 1; i >= 0; --i) writeChar(buf[i]);
+bool bfs() {
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		if (!matchU[i]) {
+			dist[i] = 0;
+			q.push(i);
+		} else
+			dist[i] = INF;
 	}
-	if (end) writeChar(end);
-}
-
-inline void writeStr(const char* s) {
-	while (*s) writeChar(*s++);
-}
-}	 // namespace IO
-
-#define N 400005
-
-vector<vector<int>> adj(N);
-int match[N + 1], dis[N + 1];
-bool visited[N + 1];
-const int inf = 1E9;
-
-void ResetVisited() {
-	for (int i = 0; i <= N; ++i) {
-		visited[i] = 0;
-	}
-}
-
-bool BFS(const vector<int>& U) {
-	queue<int> Q;
-
-	for (const auto u : U) {
-		if (match[u] == N) {
-			dis[u] = 0;
-			Q.push(u);
-		} else {
-			dis[u] = inf;
-		}
-	}
-
-	dis[N] = inf;
-
-	while (!Q.empty()) {
-		int u = Q.front();
-		Q.pop();
-		visited[u] = 1;
-		if (dis[u] < dis[N]) {
-			for (const auto v : adj[u]) {
-				if (dis[match[v]] == inf) {
-					visited[v] = 1;
-					dis[match[v]] = 1 + dis[u];
-					Q.push(match[v]);
+	dist[0] = INF;
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+		if (dist[u] < dist[0]) {
+			for (int v : adj[u]) {
+				if (dist[matchV[v]] == INF) {
+					dist[matchV[v]] = dist[u] + 1;
+					q.push(matchV[v]);
 				}
 			}
 		}
 	}
-
-	return (dis[N] != inf);
+	return dist[0] != INF;
 }
 
-bool DFS(const int u) {
-	if (u == N) {
-		return true;
-	}
-
-	for (const auto v : adj[u]) {
-		if (dis[match[v]] == 1 + dis[u]) {
-			if (DFS(match[v])) {
-				match[v] = u;
-				match[u] = v;
-				return true;
+bool dfs(int u) {
+	if (u) {
+		for (int v : adj[u]) {
+			if (dist[matchV[v]] == dist[u] + 1) {
+				if (dfs(matchV[v])) {
+					matchV[v] = u;
+					matchU[u] = v;
+					return true;
+				}
 			}
 		}
+		dist[u] = INF;
+		return false;
 	}
-
-	dis[u] = inf;
-	return false;
-}
-
-int HopcroftKarpMaximumMatching(const vector<int>& U, const vector<int>& V) {
-	for (const auto u : U) {
-		match[u] = N;
-	}
-	for (const auto v : V) {
-		match[v] = N;
-	}
-	int matching = 0;
-	for (ResetVisited(); BFS(U); ResetVisited()) {
-		for (const auto u : U) {
-			if (match[u] == N) {
-				matching += DFS(u);
-			}
-		}
-	}
-	return matching;
+	return true;
 }
 
 int main() {
-	int n, m;
-	scanf("%d%d", &n, &m);
-
-	while (m--) {
-		int i, j;
-		scanf("%d%d", &i, &j);
-		adj[i].push_back(n + j);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		in_degree[v]++;
 	}
 
-	vector<int> U(n), V(n);
-	iota(U.begin(), U.end(), 1);
-	iota(V.begin(), V.end(), n + 1);
-
-	if (HopcroftKarpMaximumMatching(U, V) < n - 2) {
-		printf("NO\n");
-		return 0;
-	}
-
-	printf("YES\n");
-
-	vector<vector<int>> splits;
-	for (int j = 1; j <= n; ++j) {
-		if (match[n + j] != N) {
-			continue;
+	// 1. Topological Sort
+	queue<int> tq;
+	for (int i = 1; i <= n; i++)
+		if (in_degree[i] == 0) tq.push(i);
+	int t_ptr = 0;
+	while (!tq.empty()) {
+		int u = tq.front();
+		tq.pop();
+		topo[t_ptr++] = u;
+		for (int v : adj[u]) {
+			if (--in_degree[v] == 0) tq.push(v);
 		}
-
-		vector<int> split{j};
-		for (int i = j; match[i] != N;) {
-			i = match[i] - n;
-			split.push_back(i);
-		}
-		splits.push_back(split);
 	}
 
-	splits.resize(2);
-
-	for (const auto& split : splits) {
-		printf("%lu ", split.size());
-		for (auto u : split) {
-			printf("%d ", u);
+	// 2. Topological Greedy Matching (Warm-up)
+	int matching = 0;
+	for (int i = 0; i < n - 1; i++) {
+		int u = topo[i];
+		int next_v = topo[i + 1];
+		// Check if edge exists between adjacent topo nodes
+		for (int v : adj[u]) {
+			if (v == next_v && !matchV[v] && !matchU[u]) {
+				matchU[u] = v;
+				matchV[v] = u;
+				matching++;
+				break;
+			}
 		}
-		printf("\n");
 	}
 
+	// 3. Complete matching with Hopcroft-Karp
+	while (bfs()) {
+		for (int i = 1; i <= n; i++)
+			if (!matchU[i] && dfs(i)) matching++;
+	}
+
+	if (n - matching > 2) {
+		cout << "NO\n";
+	} else {
+		cout << "YES\n";
+		vector<bool> is_start(n + 1, true);
+		for (int i = 1; i <= n; i++)
+			if (matchV[i]) is_start[i] = false;
+
+		vector<vector<int>> paths;
+		for (int i = 1; i <= n; i++) {
+			if (is_start[i]) {
+				vector<int> p;
+				int curr = i;
+				while (curr) {
+					p.push_back(curr);
+					curr = matchU[curr];
+				}
+				paths.push_back(p);
+			}
+		}
+		while (paths.size() < 2) paths.push_back({});
+		for (int i = 0; i < 2; i++) {
+			cout << paths[i].size();
+			for (int x : paths[i]) cout << " " << x;
+			cout << "\n";
+		}
+	}
 	return 0;
 }
